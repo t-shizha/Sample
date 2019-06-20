45183dd673050abd75b829f7274514a89518338d
shichao <t-shizha@microsoft.com>
2019-06-20 09:53:23 +0800

    first

---
+++ Sample/src/MyString.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace Sample.src
+{
+    class MyString
+    {
+        private string s = "MyString: ";
+        public MyString(string str)
+        {
+            this.s = this.s + str;
+            Console.WriteLine("Init MyString" +
+                "!!!");
+        }
+
+        public string[] Split()
+        {
+            return this.s.Split(':'); 
+        }
+
+        public string Up()
+        {
+            return 
+                this.s.ToUpper();
+        }
+
+        public string Concat(string s, int n)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                this.s =
+                    String.Concat(
+                    this.s, s
+                    );
+            }
+            return this.s;
+        }
+    }
+}
---
+++ Sample/src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using Sample.src;
+using Xunit;
+//using Nunit;
+
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assert.Equal(3, 5);
+            Assert.True(8 != 7);
+            Assert.Equal("abc", "shzz");
+            //Console.WriteLine("Bill total:\t{ 0,8:c}", 257);
+            MyString s1 = new MyString("str1");
+            MyString s2 = new MyString("str2");
+            string s = " shzz";
+
+            // test Up method
+            Console.WriteLine(s1.Up());
+
+            string s2up = s2.Up();
+            Console.WriteLine(s2up);
+
+            string[] splits = s1.Split();
+            foreach (string sp in splits)
+                Console.WriteLine(sp);
+
+            Console.WriteLine(
+                s1.Concat(s, 3)
+                );
+        }
+    }
+}
